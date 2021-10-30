e2f9d24c4d1cef8396a79c3779687651346ed976
Eduardo Nicastro <enicastro@fullstacklabs.co>
2021-10-30 15:33:49 -0300

    redirect number routes to month name route

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
+
+monthly_challenges = {
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 minutes every day!",
+    "march": "Learn Django for at least 20 minutes every day!",
+    "april": "Learn Django for at least 20 minutes every day!",
+    "may": "Learn Django for at least 20 minutes every day!",
+    "june": "Learn Django for at least 20 minutes every day!",
+    "july": "Learn Django for at least 20 minutes every day!",
+    "august": "Learn Django for at least 20 minutes every day!",
+    "september": "Learn Django for at least 20 minutes every day!",
+    "october": "Learn Django for at least 20 minutes every day!",
+    "november": "Learn Django for at least 20 minutes every day!",
+    "december": "Learn Django for at least 20 minutes every day!",
+}
 
 
-def index(request):
-    return HttpResponse("This works!")
+def monthly_challenge_by_number(request, month: int):
+    try:
+        forward_month = list(monthly_challenges.keys())[month - 1]
+    except:
+        return HttpResponseNotFound("This month is not supported!")
+
+    return HttpResponseRedirect(f'/challenges/{forward_month}')
+
+
+def monthly_challenge(request, month: str):
+    try:
+        challenge_text = monthly_challenges[month]
+    except:
+        return HttpResponseNotFound("This month is not supported!")
+    return HttpResponse(challenge_text)
